7096560345d290a8255af5ee5255169e2f2068e2
Tiago <tiagolinhares051@gmail.com>
2023-11-02 13:41:31 -0300

    ajustando crypt

--- iac/iac.sh
+++ iac/iac.sh
@@ -28,23 +28,23 @@ echo "Grupos criados com sucesso"
 echo "Criando usuários"
 
 # Usuários da área administrativa
-useradd carlos_santos -m -g grp_admin -s /bin/bash -c "Administrativo Carlos Santos" -p $(openssl passwd -crypt Senha123) 
-useradd maria_silva -m -g grp_admin -s /bin/bash -c "Administrativo Maria da Silva" -p $(openssl passwd -crypt Senha123) 
-useradd joao_pedro -m -g grp_admin -s /bin/bash -c "Administrativo João Pedro" -p $(openssl passwd -crypt Senha123) 
+useradd carlos_santos -m -g grp_admin -s /bin/bash -c "Administrativo Carlos Santos" -p $(openssl passwd -6 Senha123) 
+useradd maria_silva -m -g grp_admin -s /bin/bash -c "Administrativo Maria da Silva" -p $(openssl passwd -6 Senha123) 
+useradd joao_pedro -m -g grp_admin -s /bin/bash -c "Administrativo João Pedro" -p $(openssl passwd -6 Senha123) 
 
 echo "Criado usuários da área administrativa"
 
 # Usuários da área de vendas
-useradd debora_silva -m -g grp_ven -s /bin/bash -c "Vendas Débora da Silva" -p $(openssl passwd -crypt Senha123) 
-useradd sebastiana_maria -m -g grp_ven -s /bin/bash -c "Vendas Sebastiana Maria" -p $(openssl passwd -crypt Senha123) 
-useradd roberto_carlos -m -g grp_ven -s /bin/bash -c "Vendas Roberto Carlos" -p $(openssl passwd -crypt Senha123) 
+useradd debora_silva -m -g grp_ven -s /bin/bash -c "Vendas Débora da Silva" -p $(openssl passwd -6 Senha123) 
+useradd sebastiana_maria -m -g grp_ven -s /bin/bash -c "Vendas Sebastiana Maria" -p $(openssl passwd -6 Senha123) 
+useradd roberto_carlos -m -g grp_ven -s /bin/bash -c "Vendas Roberto Carlos" -p $(openssl passwd -6 Senha123) 
 
 echo "Criado usuários da área de vendas"
 
 # Usuários da área de segurança
-useradd josefina_silva -m -g grp_sec -s /bin/bash -c "Segurança Josefina da Silva" -p $(openssl passwd -crypt Senha123) 
-useradd amanda_silva -m -g grp_sec -s /bin/bash -c "Segurança Amanda da Silva" -p $(openssl passwd -crypt Senha123) 
-useradd rogerio_silva -m -g grp_sec -s /bin/bash -c "Segurança Rogerio da Silva" -p $(openssl passwd -crypt Senha123) 
+useradd josefina_silva -m -g grp_sec -s /bin/bash -c "Segurança Josefina da Silva" -p $(openssl passwd -6 Senha123) 
+useradd amanda_silva -m -g grp_sec -s /bin/bash -c "Segurança Amanda da Silva" -p $(openssl passwd -6 Senha123) 
+useradd rogerio_silva -m -g grp_sec -s /bin/bash -c "Segurança Rogerio da Silva" -p $(openssl passwd -6 Senha123) 
 
 echo "Criado usuários da área de segurança"
 
